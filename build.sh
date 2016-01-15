9a39af7d26df253072bac510d28ec38259c086a7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-01-15 13:11:58 +0100

    restructure + remove appcache

--- build.sh
+++ build.sh
@@ -20,8 +20,7 @@ cp -a platforms/browser/www/cordova* resources/public/
 
 # Manifest file
 
-echo "CACHE MANIFEST" > index.appcache
-echo "# `date`" >> index.appcache
-find assets -type f >> index.appcache
-echo "index.html" >> index.appcache
-echo "index.js" >> index.appcache
+#echo "CACHE MANIFEST" > index.appcache
+#echo "# `date`" >> index.appcache
+#find assets -type f >> index.appcache
+#echo "index.js" >> index.appcache
